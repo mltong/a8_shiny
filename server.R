28f38353223dd27714d8a46fa36c9ceb26a842d4
Mary Tong <askmarytong@gmail.com>
2016-02-29 22:57:20 -0800

    fourth commit

--- server.R
+++ server.R
@@ -3,13 +3,13 @@ data(iris)
 shinyServer(
   
   function(input, output) {
-    
+    # Display the data of iris_LW
     output$data <- renderTable({
       colm <- as.numeric(input$iris_LW)
       iris[colm]
     
     })
-    
+    # Display the histogram 
     output$histogram <- renderPlot({
       colm <- as.numeric(input$iris_LW)
       hist(iris[,colm], breaks=seq(0, max(iris[,colm]), l=input$bins+1), col = input$colors, main="Histogram of iris dataset", xlab=names(iris[colm]), xlim=c(0,max(iris[,colm])))
--- ui.R
+++ ui.R
@@ -7,6 +7,7 @@ shinyUI(fluidPage(
   sidebarLayout(position = "right",
     # Sidebar panel with displaying the data, selecting bins for histogram and color changing
     sidebarPanel(
+      # Widget 1: Checkbox Group 
       checkboxGroupInput("iris_LW",
                          label = "1. Select the variable(s) to display data", 
                          choices =c("Sepal.Length" = 1, 
@@ -15,12 +16,12 @@ shinyUI(fluidPage(
                                     "Petal.Width" = 4), 
                           selected = 1),
       br(),
-      
+      # Widget 2: Select the number of bins to display histogram
       sliderInput("bins", 
                   label = "Number of bins in histogram (approximate):", 
                   min = 5, max = 25, value = 15),
       br(), 
-      
+      # Widget 3: Changing colors 
       radioButtons("colors", 
                   label =  "3. Select the color of histogram", 
                   choices =c("Red", "Purple", "Yellow", "Grey"),
@@ -31,7 +32,7 @@ shinyUI(fluidPage(
     mainPanel(
       code("For data, select as many variables you like and for plot, please select only one variable to display.", style = "color: gray"),
       br(),
-
+      # Panel Tabset
       tabsetPanel(type="tab", 
                   tabPanel("Data", tableOutput("data")),
                   tabPanel("Plot", plotOutput("histogram"))
